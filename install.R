0fcc399fefcd7499c557ca70f0ad2fdfb7efe784
Ian Morelan <imorelan@campus-115-035.ucdavis.edu>
2019-10-25 16:49:34 -0700

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,7 @@
-pkgs = c("BiocManager","tidyverse", "plotly")
+pkgs = c("BiocManager","tidyverse", "plotly", "devtools")
 ncores = parallel::detectCores()
 install.packages(pkgs, Ncpus = ncores)
 
 BiocManager::install("phyloseq")
+
+devtools::install_github("katiejolly/nationalparkcolors")
